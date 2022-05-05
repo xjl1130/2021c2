fd1d248530ecb4f5d83d876d0cde03fdf1c8479c
xjl1130 <helloallenhsu@gmail.com>
2022-05-05 22:23:17 +0800

    20220505

---
+++ week10-4.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+class Record{
+public:
+    char name[30];
+    int grade;
+};
+bool compare(Record a, Record b){
+    return (a.grade > b.grade);
+}
+int main(){
+    FILE * fin = fopen("input.txt","r");
+    int N;
+    fsacnf(fin,"%d",&N);
+    std::vector<Record> sudent[N];
+    for(int i=0;i<N;i++){
+        fscanf(fin,"%s %d",student[i].name,&student[i].grade);
+    }
+    std::sort(student.begin(),student.end(),compare);
+    for(int i=0;i<N;i++){
+        printf("%s %d\n",student[i].name,student[i].grade);
+    }
+}
