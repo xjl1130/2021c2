80878b3e81de4a3974122378f4d424d626b7995e
xjl1130 <helloallenhsu@gmail.com>
2022-05-12 23:19:03 +0800

    20220512

---
+++ week11-2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+char name[100][30];
+int grade[100];
+using namespace std;
+int main(){
+	int N;
+	cin>>N;
+	for(int i=0;i<N;i++){
+		cin>>name[i]>>grade[i];
+	}
+	for(int i=0;i<N;i++){
+		cout<<name[i]<<endl;
+	}
+}
